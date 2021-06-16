748e973d7c17d163435f26ed86ebd91d7511c041
Dustin Fox <djf149@hotmail.com>
2021-06-16 16:14:56 -0400

    Add project files.

---
+++ Loop_counting_Challenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loop_counting_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int p = 1; p <=5; p++)
+            {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            }
+
+
+
+        }
+    }
+}
